f27508c3cece0f3c0861104b4d89658e7de81cdf
happy_ryo <iwama.ryo.0731@gmail.com>
2023-08-09 15:30:16 +0900

    Add OpenAI API integration and review functionality

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { Configuration, OpenAIApi } from 'openai';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -20,7 +21,53 @@ export function activate(context: vscode.ExtensionContext) {
     });
 
     context.subscriptions.push(disposable);
+
 }
+
+async function getReview(selectedText: string): Promise<string> {
+    const prompt = `This is a review of the following text: "${selectedText}"\n\nReview:`;
+
+    const configuration = new Configuration({
+        apiKey: getOpenAiApiKey(),
+    });
+
+    const openai = new OpenAIApi(configuration);
+
+    const chatCompletion = await openai.createChatCompletion({
+        model: "gpt-4",
+        messages: [{ role: "user", content: prompt}],
+    });
+
+    const review = chatCompletion.data.choices[0].message?.content;
+
+    if (review === undefined) {
+        vscode.window.showErrorMessage('OpenAI API response is undefined');
+        throw new Error('OpenAI API response is undefined');
+    } else {
+        if (typeof review !== 'string') {
+            vscode.window.showErrorMessage('OpenAI API response is not a string');
+            throw new Error('OpenAI API response is not a string');
+        }
+        return review;
+    }
+}
+
+function getSelectedText(): string {
+    const editor = vscode.window.activeTextEditor;
+    if (editor === undefined) {
+        vscode.window.showErrorMessage('No active editor');
+        throw new Error('No active editor');
+    } else {
+        const selectedText = editor.document.getText(editor.selection);
+        if (selectedText === '') {
+            vscode.window.showErrorMessage('No text selected');
+            throw new Error('No text selected');
+        } else {
+            return selectedText;
+        }
+    }
+}
+
 
 function getOpenAiApiKey(): string {
     const config = vscode.workspace.getConfiguration('gpt-one-click-review');
