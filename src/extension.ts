ea76b1782456d9bd9e2ccf565566102e11026e08
happy_ryo <iwama.ryo.0731@gmail.com>
2023-08-12 08:43:06 +0900

    Remove unnecessary "Hello World" command and console.log statement

--- src/extension.ts
+++ src/extension.ts
@@ -5,25 +5,7 @@ import { getReview } from './apiManager';
 import { startLoading } from './webViewManager';
 import * as path from 'path';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "gpt-one-click-review" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('gpt-one-click-review.helloWorld', () => {
-        // The code you place here will be executed every time your command is executed
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World from GPT One Click Review!');
-    });
-
-    context.subscriptions.push(disposable);
-
     subscriptionReviewCode(context, 'gpt-4');
     subscriptionReviewCode(context, 'gpt-3.5-turbo-16k');
 }
